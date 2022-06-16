6636ed0e55f63914f4e7b420cfda24397e558779
Nico R <nico.roessel@padis.io>
2022-06-16 23:11:27 +0200

    readded outcommented test

--- src/CoinCombo/coinCombo.test.ts
+++ src/CoinCombo/coinCombo.test.ts
@@ -1,10 +1,10 @@
 import coinCombo from "./coinCombo";
 
 describe('Testing Coin Combo', () => {
-  //
-  // it('should output', () => {
-  //   expect(coinCombo([2,3,5,7], 17).sort()).toEqual([3,7,7].sort());
-  // });
+
+  it('should output', () => {
+    expect(coinCombo([2,3,5,7], 17).sort()).toEqual([3,7,7].sort());
+  });
 
   it('should output', () => {
     expect(coinCombo([2,3,5], 11).sort()).toEqual([3,3,5].sort());
